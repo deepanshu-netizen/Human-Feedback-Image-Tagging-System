b1734a72db89d60387d2c72e7ad19858ecf4c5c3
deepanshu <djain63@asu.edu>
2026-03-31 16:08:49 -0700

    Vercel Setup

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500",
+        "http://localhost:5500"
+        "https://your-vercel-domain.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
